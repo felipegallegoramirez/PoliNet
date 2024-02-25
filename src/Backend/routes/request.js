0ead271b4671339cb5bf007087f464ad01a6dffd
Felipe Gallego <felipegallego600@gmail.com>
2024-02-25 13:11:35 -0500

    backend_crud_routes

---
+++ src/Backend/routes/request.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const request = require("../controllers/request.controller")
+
+router.get("/", request.getRequests);
+router.post("/", request.createRequest); 
+router.get("/:id", request.getRequest); 
+router.put("/:id",request.editRequest);
+router.delete("/:id", request.deleteRequest);
+
+
+
+module.exports = router 
