04892a27eef53c28b9908bfd243a60d2c4c72e0a
Felipe Gallego <felipegallego600@gmail.com>
2024-05-10 14:08:15 -0500

    hash

--- src/Frontend/src/app/app.config.ts
+++ src/Frontend/src/app/app.config.ts
@@ -1,12 +1,13 @@
-import { ApplicationConfig} from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), provideAnimationsAsync()],
-
+  providers: [
+    provideRouter(routes, withHashLocation()), // Enable hash routing
+    provideHttpClient(),
+    provideAnimationsAsync()
+  ],
 };
-
