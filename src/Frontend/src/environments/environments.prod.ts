5f64da9f97260df44935aa5cfd42ac294cd6494e
Felipe Gallego <felipegallego600@gmail.com>
2024-05-10 21:35:52 -0500

    casi

--- src/Frontend/src/environments/environments.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    baseUrl: "https://polinet.netlify.app/",
-    backend:"https://polinet.onrender.com/"
-  };
--- src/Frontend/src/environments/environments.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    baseUrl: "https://polinet.netlify.app/",
-    backend:"https://polinet.onrender.com/"
-  };
